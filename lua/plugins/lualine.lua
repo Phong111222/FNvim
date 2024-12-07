ae74f48cfa31820d988e522cf9db41b60f23119d
Frank Nguyen <frank.nguyen@drova.com>
2024-12-07 23:10:31 +0700

    update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -24,7 +24,7 @@ return {
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { { "filename", path = 3 } },
+				lualine_c = { "filename" },
 				lualine_x = { "encoding", "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
