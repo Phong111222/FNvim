8ac06f7318b5b4b55bf47dfef9c15de4c980be32
Frank Nguyen <frank.nguyen@drova.com>
2024-12-13 00:19:22 +0700

    add nvim-ts-autotag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,39 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
+	{
+		"windwp/nvim-ts-autotag",
+		event = "InsertEnter",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
 
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
+		config = function()
+			local treesitter = require("nvim-treesitter.configs")
 
-		treesitter.setup({
-			ensure_installed = {
-				"luadoc",
-				"printf",
-				"vim",
-				"vimdoc",
-				"typescript",
-				"javascript",
-				"html",
-				"vue",
-				"svelte",
-				"tsx",
-				"lua",
-			},
+			require("nvim-ts-autotag").setup()
+			treesitter.setup({
+				ensure_installed = {
+					"luadoc",
+					"printf",
+					"vim",
+					"vimdoc",
+					"typescript",
+					"javascript",
+					"html",
+					"vue",
+					"svelte",
+					"tsx",
+					"lua",
+				},
 
-			highlight = {
-				enable = true,
-				use_languagetree = true,
-			},
+				highlight = {
+					enable = true,
+					use_languagetree = true,
+				},
 
-			indent = { enable = true },
-			autotag = { enable = true },
-		})
-	end,
+				indent = { enable = true },
+				-- autotag = { enable = true },
+			})
+		end,
+	},
 }
