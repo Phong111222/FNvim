6a818c14580e02935fd89697e8314dc8010c9a5c
Frank Nguyen <frank.nguyen@drova.com>
2024-12-31 12:41:17 +0700

    update nvim-tree

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,49 +1,111 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
+    -- stylua: ignore
+    local colors = {
+      blue   = '#80a0ff',
+      cyan   = '#79dac8',
+      black  = '#080808',
+      white  = '#c6c6c6',
+      red    = '#ff5189',
+      violet = '#d183e8',
+      grey   = '#303030',
+    }
+		local bubbles_theme = {
+			normal = {
+				a = { fg = colors.black, bg = colors.violet },
+				b = { fg = colors.white, bg = colors.grey },
+				c = { fg = colors.white },
+			},
+
+			insert = { a = { fg = colors.black, bg = colors.blue } },
+			visual = { a = { fg = colors.black, bg = colors.cyan } },
+			replace = { a = { fg = colors.black, bg = colors.red } },
+
+			inactive = {
+				a = { fg = colors.white, bg = colors.black },
+				b = { fg = colors.white, bg = colors.black },
+				c = { fg = colors.white },
+			},
+		}
+
 		require("lualine").setup({
 			options = {
-				icons_enabled = true,
-				theme = "codedark",
-				section_separators = { left = "", right = "" },
-				component_separators = { left = "", right = "" },
-				disabled_filetypes = {
-					statusline = {},
-					winbar = {},
-				},
-				ignore_focus = {},
-				always_divide_middle = true,
-				always_show_tabline = true,
-				globalstatus = false,
-				refresh = {
-					statusline = 100,
-					tabline = 100,
-					winbar = 100,
-				},
+				theme = bubbles_theme,
+				component_separators = "",
+				section_separators = { left = "", right = "" },
 			},
 			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { {
-					"filename",
-					color = { fg = "#fffff" },
-				} },
-				lualine_c = { "branch", "diff", "diagnostics" },
-				lualine_x = { "encoding", "fileformat", "filetype" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
+				lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+				lualine_b = { "filename", "branch", "diagnostics", "diff" },
+				lualine_c = {
+					"%=", --[[ add your center compoentnts here in place of this comment ]]
+				},
+				lualine_x = {},
+				lualine_y = { "filetype", "progress" },
+				lualine_z = {
+					{ "location", separator = { right = "" }, left_padding = 2 },
+				},
+
+				-- 		lualine_c = { "branch", "diff", "diagnostics" },
+				-- 		lualine_x = { "encoding", "fileformat", "filetype" },
+				-- 		lualine_y = { "progress" },
+				-- 		lualine_z = { "location" },
 			},
 			inactive_sections = {
-				lualine_a = {},
+				lualine_a = { "filename" },
 				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
+				lualine_c = {},
+				lualine_x = {},
 				lualine_y = {},
-				lualine_z = {},
+				lualine_z = { "location" },
 			},
 			tabline = {},
-			winbar = {},
-			inactive_winbar = {},
 			extensions = {},
 		})
+		-- require("lualine").setup({
+		-- 	options = {
+		-- 		icons_enabled = true,
+		-- 		theme = "codedark",
+		-- 		section_separators = { left = "", right = "" },
+		-- 		component_separators = { left = "", right = "" },
+		-- 		disabled_filetypes = {
+		-- 			statusline = {},
+		-- 			winbar = {},
+		-- 		},
+		-- 		ignore_focus = {},
+		-- 		always_divide_middle = true,
+		-- 		always_show_tabline = true,
+		-- 		globalstatus = false,
+		-- 		refresh = {
+		-- 			statusline = 100,
+		-- 			tabline = 100,
+		-- 			winbar = 100,
+		-- 		},
+		-- 	},
+		-- 	sections = {
+		-- 		lualine_a = { "mode" },
+		-- 		lualine_b = { {
+		-- 			"filename",
+		-- 			color = { fg = "#fffff" },
+		-- 		} },
+		-- 		lualine_c = { "branch", "diff", "diagnostics" },
+		-- 		lualine_x = { "encoding", "fileformat", "filetype" },
+		-- 		lualine_y = { "progress" },
+		-- 		lualine_z = { "location" },
+		-- 	},
+		-- 	inactive_sections = {
+		-- 		lualine_a = {},
+		-- 		lualine_b = {},
+		-- 		lualine_c = { "filename" },
+		-- 		lualine_x = { "location" },
+		-- 		lualine_y = {},
+		-- 		lualine_z = {},
+		-- 	},
+		-- 	tabline = {},
+		-- 	winbar = {},
+		-- 	inactive_winbar = {},
+		-- 	extensions = {},
+		-- })
 	end,
 }
