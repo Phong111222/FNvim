365de560e5f03e01e5251e7838c7e1bacbf5ca3e
frank.nguyen <frank.nguyen@drova.com>
2025-10-09 22:17:17 +0700

    update config

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -1,47 +1,47 @@
 local M = {}
 
 function PromptSaveBeforeClose()
-	if vim.bo.modified then
-		local choice = vim.fn.confirm("Save changes before closing?", "&Yes\n&No\n&Cancel")
-		if choice == 1 then
-			vim.cmd("w|bd") -- Save the file
-		elseif choice == 2 then
-			if #vim.api.nvim_list_wins() == 1 then
-				vim.cmd("enew") -- Quit Neovim if it's the last window
-			else
-				vim.cmd("close") -- Close the current window
-			end
-		else
-			return -- Cancel the action
-		end
-	else
-		if #vim.api.nvim_list_wins() == 1 then
-			vim.cmd("enew") -- Quit Neovim if it's the last window
-		else
-			vim.cmd("close") -- Close the current window
-		end
-	end
+  if vim.bo.modified then
+    local choice = vim.fn.confirm("Save changes before closing?", "&Yes\n&No\n&Cancel")
+    if choice == 1 then
+      vim.cmd("w|bd") -- Save the file
+    elseif choice == 2 then
+      if #vim.api.nvim_list_wins() == 1 then
+        vim.cmd("enew") -- Quit Neovim if it's the last window
+      else
+        vim.cmd("close") -- Close the current window
+      end
+    else
+      return -- Cancel the action
+    end
+  else
+    if #vim.api.nvim_list_wins() == 1 then
+      vim.cmd("enew") -- Quit Neovim if it's the last window
+    else
+      vim.cmd("close") -- Close the current window
+    end
+  end
 end
 
 function M.setup()
-	local opts = { noremap = true, silent = true }
-	local map = vim.keymap.set
-	vim.api.nvim_set_keymap("i", "jk", "<ESC>", opts)
-	map("t", "<esc><esc>", "<c-\\><c-n>", opts)
-	vim.api.nvim_set_keymap(
-		"n",
-		"<leader>x",
-		":lua PromptSaveBeforeClose()<CR>",
-		{ noremap = true, silent = true, desc = "Close Current Buffer" }
-	)
-	map("n", "<leader>a", "G$vgg0", { noremap = true, silent = true, desc = "Select All" })
-	map("v", ">", ">gv", { desc = "Indent and reselect" }) -- Indent and reselect
-	map("v", "<", "<gv", { desc = "Unindent and reselect" }) -- Unindent and reselect
+  local opts = { noremap = true, silent = true }
+  local map = vim.keymap.set
+  vim.api.nvim_set_keymap("i", "jk", "<ESC>", opts)
+  map("t", "<esc><esc>", "<c-\\><c-n>", opts)
+  vim.api.nvim_set_keymap(
+    "n",
+    "<leader>x",
+    ":lua PromptSaveBeforeClose()<CR>",
+    { noremap = true, silent = true, desc = "Close Current Buffer" }
+  )
+  map("n", "<leader>a", "G$vgg0", { noremap = true, silent = true, desc = "Select All" })
+  map("v", ">", ">gv", { desc = "Indent and reselect" })  -- Indent and reselect
+  map("v", "<", "<gv", { desc = "Unindent and reselect" }) -- Unindent and reselect
 
-	map("n", "<leader>W", ":noautocmd w<CR>", { desc = "Save without formatting" })
-	map("v", "J", ":m '>+1<CR>gv=gv", opts)
-	map("v", "K", ":m '>-2<CR>gv=gv", opts)
-	map("v", "<leader>r", '"hy:%s/<C-r>h//g<Left><Left>', { noremap = true, silent = true, desc = "Replace Selection" })
+  map("n", "<leader>W", ":noautocmd w<CR>", { desc = "Save without formatting" })
+  map("v", "J", ":m '>+1<CR>gv=gv", opts)
+  map("v", "K", ":m '>-2<CR>gv=gv", opts)
+  map("v", "<leader>r", '"hy:%s/<C-r>h//g<Left><Left>', { noremap = true, silent = true, desc = "Replace Selection" })
 end
 
 return M
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,42 +1,44 @@
 return {
-	{
-		"windwp/nvim-ts-autotag",
-		event = "InsertEnter",
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
 
-		config = function()
-			local treesitter = require("nvim-treesitter.configs")
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
 
-			require("nvim-ts-autotag").setup()
-			treesitter.setup({
-				ensure_installed = {
-					"luadoc",
-					"printf",
-					"vim",
-					"vimdoc",
-					"typescript",
-					"javascript",
-					"html",
-					"vue",
-					"svelte",
-					"tsx",
-					"lua",
-					"go",
-					"graphql",
-					"python",
-				},
+      require("nvim-ts-autotag").setup()
 
-				highlight = {
-					enable = true,
-					use_languagetree = true,
-				},
+      treesitter.setup({
+        auto_install = true,
+        ensure_installed = {
+          "luadoc",
+          "printf",
+          "vim",
+          "vimdoc",
+          "typescript",
+          "javascript",
+          "html",
+          "vue",
+          "svelte",
+          "tsx",
+          "lua",
+          "go",
+          "graphql",
+          "python",
+        },
 
-				indent = { enable = true },
-				-- autotag = { enable = true },
-			})
-		end,
-	},
+        highlight = {
+          enable = true,
+          use_languagetree = true,
+        },
+
+        indent = { enable = true },
+        -- autotag = { enable = true },
+      })
+    end,
+  },
 }
